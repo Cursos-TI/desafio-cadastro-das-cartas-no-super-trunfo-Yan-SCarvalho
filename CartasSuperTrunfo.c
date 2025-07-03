691546676b20a13a2fdf6a4031f1a95777f8e3bf
Yan Carvalho <yan.carvalho.profissional@gmail.com>
2025-07-03 04:40:10 +0000

    TODOS_DESAFIOS_FEITOS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,18 +3,17 @@
 int main() {
     // Variáveis da primeira carta
     char codigo1[4];
-    char nome1[20]; // Nome da cidade 1 (sem espaços)
+    char nome1[20];
     int populacao1;
     float area1;
     float pib1;
     int pontos_turisticos1;
-
     float densidade_populacional1;
     float pib_per_capita1;
 
     // Variáveis da segunda carta
     char codigo2[4];
-    char nome2[20]; // Nome da cidade 2 (sem espaços)
+    char nome2[20];
     int populacao2;
     float area2;
     float pib2;
@@ -54,35 +53,35 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
 
-    // Saída de dados das cartas
+    // NÍVEL AVENTUREIRO
+    densidade_populacional1 = populacao1 / area1;
+    pib_per_capita1 = pib1 / populacao1;
+
+    densidade_populacional2 = populacao2 / area2;
+    pib_per_capita2 = pib2 / populacao2;
+
+    // Saída da Carta 1
     printf("\n--- Carta 1 ---\n");
     printf("Código: %s\n", codigo1);
     printf("Nome da cidade: %s\n", nome1);
     printf("População: %d\n", populacao1);
-    printf("Área: %.2fkm²\n", area1);
+    printf("Área: %.2f km²\n", area1);
     printf("PIB: R$ %.2f\n", pib1);
     printf("Pontos turísticos: %d\n", pontos_turisticos1);
-   
-    densidade_populacional1 = (float)(populacao1 / area1);
-    printf("Densidade_populacional: %1.f\n" , densidade_populacional1);
-    pib_per_capita1 = (float)(pib1 / populacao1);
-    printf("Pib per capita é: %1.f\n" , pib_per_capita1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional1);
+    printf("PIB per capita: R$ %.2f\n", pib_per_capita1);
 
-
+    // Saída da Carta 2
     printf("\n--- Carta 2 ---\n");
     printf("Código: %s\n", codigo2);
     printf("Nome da cidade: %s\n", nome2);
     printf("População: %d\n", populacao2);
-    printf("Área: %.2fkm²\n" , area2);
+    printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f\n", pib2);
     printf("Pontos turísticos: %d\n", pontos_turisticos2);
-   
-    //Nível Aventureiro
-    densidade_populacional2 = (float)(populacao2 / area2);
-    printf("Densidade_populaciona2: %1.f\n" , densidade_populacional2);
-    pib_per_capita2 = (float)(pib2 / populacao2);
-    printf("Pib per capita é: %1.f\n" , pib_per_capita2);
-    
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional2);
+    printf("PIB per capita: R$ %.2f\n", pib_per_capita2);
 
     return 0;
 }
+
